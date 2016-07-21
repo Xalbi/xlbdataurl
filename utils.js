05bc5c54863e565d38bf7e63f895bf215440b252
Ahmed CHELBI <ahmad.chelbi@gmail.com>
2016-07-21 16:03:09 +0200

    Update utils.js
    Correction #2

--- utils.js
+++ utils.js
@@ -7,32 +7,36 @@
  */
 
 
+
 exports.parseURL = function(url) {
 
   if (url && url.indexOf('?') > 0) {
-
     var dataString = url.split('?')[1]
     var urlString = url.split('?')[0]
     var dataStringParsed = ''
 
-    try {
-      var arrayParam = dataString.split("&")
-      var strRes = ''
-      var resultArray = []
-      arrayParam.forEach(function (elm) {
-        var arrayElm = elm.split('=')
-          if (arrayElm.length > 2) {
-          var attr = arrayElm.shift()
-          resultArray.push(attr+':'+arrayElm.join('='))
-        }else {
-          resultArray.push(arrayElm.join(':'))
-        }
-        dataStringParsed = '{"'+resultArray.join('","').split(':').join('":"')+'"}'
-      })
-     } catch (e) {
-       console.error(e);
-       return;
-     }
+      try {
+        var arrayParam = dataString.split("&")
+        var strRes = ''
+        var resultArray = []
+
+        arrayParam.forEach(function (elm) {
+          var arrayElm = elm.split('=')
+
+            if (arrayElm.length > 2) {
+              var attr = arrayElm.shift()
+              resultArray.push(attr+'":"'+arrayElm.join('='))
+            }else {
+              resultArray.push(arrayElm.join('":"'))
+            }
+
+            dataStringParsed = '{"'+resultArray.join('","')+'"}'
+        })
+      } catch (e) {
+        console.error(e);
+        return;
+      }
+
    return {url : urlString, data : JSON.parse(dataStringParsed)}
   }else{
     return {url : url, data : undefined }
